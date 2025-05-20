479f05c5b873a0c595b72a4b0e85cdb5545e1f7b
Nicholas Walton <110708935+nicwalt@users.noreply.github.com>
2025-05-20 17:21:31 +1000

    catkin

--- catkin_project/catkin_workspace/devel/local_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# generated from catkin/cmake/template/local_setup.sh.in
-
-# since this file is sourced either use the provided _CATKIN_SETUP_DIR
-# or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/student/Documents/GitHub/AI-ROS-Group15/catkin_project/catkin_workspace/devel}
-CATKIN_SETUP_UTIL_ARGS="--extend --local"
-. "$_CATKIN_SETUP_DIR/setup.sh"
-unset CATKIN_SETUP_UTIL_ARGS
